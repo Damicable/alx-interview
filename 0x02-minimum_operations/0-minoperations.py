a4601f66fa9f90f61e7c22d10dc207eb7f80e9a1
Damicable <annobidiegwu@gmail.com>
2024-01-16 04:45:47 -0800

    Targeted

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,19 +9,19 @@ def minOperations(n):
     @n: The number of H characters
     Returns: 0, otherwise the fewest number of operations
     """
-    if n <= 1:
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-
-    # Initialize factors list
-    factors = []
-
-    # Find prime factors of n
-    for i in range(2, n + 1):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-
-    # Calculate minimum operations
-    min_operations = sum(factors)
-
-    return min_operations
+    min_ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            min_ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return min_ops
