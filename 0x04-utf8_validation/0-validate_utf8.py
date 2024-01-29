a3c9a993fce158ce4b0a7a89f20cf131b840a320
Damicable <annobidiegwu@gmail.com>
2024-01-29 14:06:50 -0800

    Utf validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """
+    validUTF8 - Method that determines if a given data set represents
+    a valid UTF-8 encoding.
+    @data: List of integers
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
